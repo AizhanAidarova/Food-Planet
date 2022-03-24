07110845855299b1ec5c30f1b6ba5cf97d7f7124
hellomeerim <eg.meerim@gmail.com>
2022-03-24 08:29:15 +0600

    TEST

---
+++ src/AdminPage/AdminApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminApp = () => {
+    return (
+        <div style={{maxWidth: "1600px"}}>
+            <h1>Hello, girls!</h1>
+        </div>
+    );
+};
+
+export default AdminApp;
