d30921d7ab1cf37d8ca0c8c772d877e04ab110bb
Aizhan <aizh.aidarova@gmail.com>
2022-03-27 16:05:50 +0600

    feat:add Aigerim's pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import PagesMenu from "../src/Pages/Menu/PagesMenu";
+import PagesHeader from "./Pages/Header/PagesHeader";
+import PagesMain from "./Pages/Main/PagesMain";
+import Novelties from "./Pages/Novelties/Novelties";
 
 const App = () => {
     return (
         <div>
+            <PagesHeader/>
+            <PagesMain/>
+            <Novelties/>
             <PagesMenu/>
         </div>
     );
