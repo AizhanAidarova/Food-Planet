3b4272b7840458bacb5686ebb3cfbd8b87df0b0a
hellomeerim <eg.meerim@gmail.com>
2022-03-28 17:00:47 +0600

    test

--- src/Pages/AboutUs/PagesAboutUs.jsx
+++ src/Pages/AboutUs/PagesAboutUs.jsx
@@ -1,41 +1,28 @@
-import React from 'react';
-import styles from "./PagesAboutUs.module.css"
-import aboutUs from "../Media/Icons/AboutUs1.svg"
+import React from "react";
+import styles from "./PagesAboutUs.module.css";
+import { about } from "../../Constant/Constant";
 
 const PagesAboutUs = () => {
-    return (
-        <>
-        <div style={{maxWidth: "1600px"}} className={styles.aboutUsWidth}>
-            <div style={{maxWidth: "1120px"}}>
-                <h3>Почему выбирают нас:</h3>
-            <div className={styles.fourAboutUs}>
-                <div className={styles.aboutUs}>
-                    <img src={aboutUs} alt="" />
-                    <h2>Мгновенная доставка</h2>
-                    <p>Доставим заказанную вами еду за 30 минут + напиток в подарок</p>
-                </div>
-                <div className={styles.aboutUs}>
-                    <img src={aboutUs} alt="" />
-                    <h2>Мгновенная доставка</h2>
-                    <p>Доставим заказанную вами еду за 30 минут + напиток в подарок</p>
-                </div>
-                <div className={styles.aboutUs}>
-                    <img src={aboutUs} alt="" />
-                    <h2>Мгновенная доставка</h2>
-                    <p>Доставим заказанную вами еду за 30 минут + напиток в подарок</p>
-                </div>
-                <div className={styles.aboutUs}>
-                    <img src={aboutUs} alt="" />
-                    <h2>Мгновенная доставка</h2>
-                    <p>Доставим заказанную вами еду за 30 минут + напиток в подарок</p>
-                </div>
-            </div>
-            
-               
+  return (
+    <>
+      <div style={{ maxWidth: "1600px" }} className={styles.aboutUsWidth}>
+        <div style={{ maxWidth: "1120px" }}>
+          <h3>Почему выбирают нас:</h3>
+          <div className={styles.fourAboutUs}>
+            {about.map((item) => {
+              return (
+                  <div className={styles.aboutUs}>
+                    <img src={item.img} alt="" />
+                    <h2>{item.reasons}</h2>
+                    <p>{item.reasonsInDetail}</p>
+                  </div>
+              );
+            })}
+          </div>
         </div>
-        </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default PagesAboutUs;
--- src/Pages/Feedbacks/PagesFeedbacks.jsx
+++ src/Pages/Feedbacks/PagesFeedbacks.jsx
@@ -3,6 +3,7 @@ import styles from "./PagesFeedbacks.module.css";
 import border from "../Media/Icons/FeedbackBorder.svg";
 import greyEllipse from "../Media/Icons/GreyEllipse.svg";
 import pencil from "../Media/Icons/Pencil.svg";
+import { feedbacks } from "../../Constant/Constant";
 
 const PagesFeedbacks = () => {
   return (
@@ -11,74 +12,21 @@ const PagesFeedbacks = () => {
         <div style={{ maxWidth: "1120px" }}>
           <h3>Отзывы</h3>
           <div className={styles.fourFeedbacks}>
-{/* first */}
-            <div className={styles.oneFeedback}>
-              <img src={border} />
+            {feedbacks.map((item) => {
+              return <div className={styles.oneFeedback}>
+              <img src={border}/>
               <div className={styles.firstFeedback}>
                 <img src={greyEllipse} alt="" />
                 <div className={styles.name}>
-                  <h4>Cергей</h4>
+                  <h4>{item.name}</h4>
                   <img src={pencil} alt="" />
                 </div>
-                <p>
-                  Большое спасибо! 5 пицц доставили даже быстрее чем
-                  планировалось. Пицца была горячая и всему коллективу
-                  понравилась! День Рождения прошел отлично! :)
-                </p>
-                <h4>02.07.2020</h4>
+                <p>{item.feedback}</p>
+                <h4>{item.dateOfPublication}</h4>            
               </div>
             </div>
-{/* second */}
-            <div className={styles.oneFeedback}>
-              <img src={border} />
-              <div className={styles.firstFeedback}>
-                <img src={greyEllipse} alt="" />
-                <div className={styles.name}>
-                  <h4>Cергей</h4>
-                  <img src={pencil} alt="" />
-                </div>
-                <p>
-                  Большое спасибо! 5 пицц доставили даже быстрее чем
-                  планировалось. Пицца была горячая и всему коллективу
-                  понравилась! День Рождения прошел отлично! :)
-                </p>
-                <h4>02.07.2020</h4>
-              </div>
-            </div>
-{/* third */}
-            <div className={styles.oneFeedback}>
-              <img src={border} />
-              <div className={styles.firstFeedback}>
-                <img src={greyEllipse} alt="" />
-                <div className={styles.name}>
-                  <h4>Cергей</h4>
-                  <img src={pencil} alt="" />
-                </div>
-                <p>
-                  Большое спасибо! 5 пицц доставили даже быстрее чем
-                  планировалось. Пицца была горячая и всему коллективу
-                  понравилась! День Рождения прошел отлично! :)
-                </p>
-                <h4>02.07.2020</h4>
-              </div>
-            </div>
-{/* fourth */}
-            <div className={styles.oneFeedback}>
-              <img src={border} />
-              <div className={styles.firstFeedback}>
-                <img src={greyEllipse} alt="" />
-                <div className={styles.name}>
-                  <h4>Cергей</h4>
-                  <img src={pencil} alt="" />
-                </div>
-                <p>
-                  Большое спасибо! 5 пицц доставили даже быстрее чем
-                  планировалось. Пицца была горячая и всему коллективу
-                  понравилась! День Рождения прошел отлично! :)
-                </p>
-                <h4>02.07.2020</h4>
-              </div>
-            </div>
+            })}
+      
           </div>
         </div>
       </div>
