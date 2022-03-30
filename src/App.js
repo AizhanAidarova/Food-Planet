88b838b445c5f82ca467fd8877609ff8e79c4e00
Aizhan <aizh.aidarova@gmail.com>
2022-03-30 11:59:43 +0600

    feat:merge branch

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import AllPages from "./Pages/AllPages/AllPages";
 import DeliveryMain from "./Pages/Delivery/DeliveryMain";
 
 
-import PagesMenu from "../src/Pages/Menu/PagesMenu";
-import React from 'react';
 import PagesFooter from './Pages/Footer/PagesFooter';
 import PagesAboutUs from './Pages/AboutUs/PagesAboutUs';
 import PagesFeedbacks from './Pages/Feedbacks/PagesFeedbacks';
-
-import PagesHeader from "./Pages/Header/PagesHeader";
-import PagesMain from "./Pages/Main/PagesMain";
-import Novelties from "./Pages/Novelties/Novelties";
 
 const App = () => {
     return (
@@ -24,9 +18,6 @@ const App = () => {
             </Routes>
         </BrowserRouter>
 
-            <PagesHeader/>
-            <PagesMain/>
-            <Novelties/>
 
 
     <div style={{maxWidth: "1600px"}}>
