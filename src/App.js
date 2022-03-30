0cad2097773b159283f13c7f06f6bb909494645e
Aizhan <aizh.aidarova@gmail.com>
2022-03-30 12:14:26 +0600

    feat:merge

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import AllPages from "./Pages/AllPages/AllPages";
 import DeliveryMain from "./Pages/Delivery/DeliveryMain";
 
+import PagesFooter from './Pages/Footer/PagesFooter';
+import PagesAboutUs from './Pages/AboutUs/PagesAboutUs';
+import PagesFeedbacks from './Pages/Feedbacks/PagesFeedbacks';
 
 const App = () => {
     return (
+        <>
         <BrowserRouter>
+
             <Routes>
                 <Route path="index" element={<AllPages/>}/>
                 <Route path="delivery" element={<DeliveryMain/>}/>
             </Routes>
         </BrowserRouter>
-    );
-};
+
+
+
+    <div style={{maxWidth: "1600px"}}>
+        <PagesAboutUs/>
+        <PagesFeedbacks/>
+        <PagesFooter/>
+    </div>
+          </>
+  );
+}
+
 export default App;
