97dd3ef241932b51e6b21c170f9ff4ac1db3ef09
aigerim <aigerimbekbolotova7@gmail.com>
2022-04-01 09:36:18 +0600

    feat:fixed

--- src/Pages/NoveltiesMenu/Burgers.jsx
+++ src/Pages/NoveltiesMenu/Burgers.jsx
@@ -13,27 +13,26 @@ const Burgers = (props) => {
     }
     return (
         <>
-         
+
             <div className={style.noveltiesMenu}>
                 <div className={style.cheeseburger}>
-                        <img className={style.burger} src={props.imageUrl} alt="" />
-                        <div>
-                            <h3>{props.title}</h3>
-                            <p>{props.titleDesc}</p>
-                            <p><b>{props.desc}</b></p> 
-                        </div>
-                        <div className={style.btn}>
-                            <button className={style.btn1} onClick={increment}>-</button>
-                            <p>{number}</p>
-                            <button className={style.btn2} onClick={decrement}>+</button>
-                        </div>
-                        <button className={style.btn3}>В КОРЗИНУ</button>
+                    <img className={style.burger} src={props.imageUrl} alt="" />
+                    <div>
+                        <h3>{props.title}</h3>
+                        <p className={style.noveltiesMenuP}>{props.titleDesc}</p>
+                        <p><b>{props.desc}</b></p>
+                    </div>
+                    <div className={style.btn}>
+                        <button className={style.btn1} onClick={increment}>-</button>
+                        <p>{number}</p>
+                        <button className={style.btn2} onClick={decrement}>+</button>
+                    </div>
+                    <button className={style.btn3}>В КОРЗИНУ</button>
                 </div>
             </div>
-            
+
         </>
     );
 };
 
 export default Burgers;
-           
