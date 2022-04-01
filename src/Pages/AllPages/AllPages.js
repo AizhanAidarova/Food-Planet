de9e5afd41150ea44af13539c87f029ae2ad5a91
hellomeerim <eg.meerim@gmail.com>
2022-04-01 11:20:08 +0600

    fix

--- src/Pages/AllPages/AllPages.js
+++ src/Pages/AllPages/AllPages.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PagesHeader from '../Header/PagesHeader';
 import PagesMain from '../Main/PagesMain';
 import Total from '../NoveltiesMenu/Total';
+import PagesFeedbacks from '../Feedbacks/PagesFeedbacks';
+import PagesFooter from '../Footer/PagesFooter';
+import PagesAboutUs from '../AboutUs/PagesAboutUs';
 
 
 
@@ -11,6 +14,9 @@ const App = () => {
             <PagesHeader/>
             <PagesMain/>
             <Total/>
+            <PagesAboutUs/>
+            <PagesFeedbacks/>
+            <PagesFooter/>
          </div>
     );
 };
