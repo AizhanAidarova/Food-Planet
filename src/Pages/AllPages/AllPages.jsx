4c3434686414087c30e7afbdca4e495882e7a959
Aizhan <aizh.aidarova@gmail.com>
2022-04-01 09:33:31 +0600

    feat:fix

--- src/Pages/AllPages/AllPages.jsx
+++ src/Pages/AllPages/AllPages.jsx
@@ -3,6 +3,9 @@ import PagesHeader from "../Header/PagesHeader";
 import PagesMain from "../Main/PagesMain";
 import Novelties from "../Novelties/Novelties";
 import PagesMenu from "../Menu/PagesMenu";
+import PagesFooter from "../Footer/PagesFooter";
+import PagesAboutUs from "../AboutUs/PagesAboutUs";
+import PagesFeedbacks from "../Feedbacks/PagesFeedbacks";
 
 const AllPages = () => {
     return (
@@ -12,6 +15,9 @@ const AllPages = () => {
                 <PagesMain/>
                 <Novelties/>
                 <PagesMenu/>
+                <PagesAboutUs/>
+                <PagesFeedbacks/>
+                <PagesFooter/>
             </div>
         </div>
     );
