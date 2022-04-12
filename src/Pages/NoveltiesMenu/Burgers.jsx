c7eea08c5d3767d72b703be8d40f3a520f714854
aigerim <aigerimbekbolotova7@gmail.com>
2022-04-12 16:00:12 +0600

    feat:fixed

--- src/Pages/NoveltiesMenu/Burgers.jsx
+++ src/Pages/NoveltiesMenu/Burgers.jsx
@@ -1,8 +1,8 @@
 import style from '../NoveltiesMenu/Novelties.module.css';
-import { useState } from "react";
+import {LOCALHOST_URL} from '../../AdminPage/Constant/index';
+import React, {useState,useEffect} from 'react';
 
-
-const Burgers = (props) => {
+const Burgers = () => {
     const [number, setNumber] = useState(0);
 
     const increment = () => {
@@ -11,24 +11,47 @@ const Burgers = (props) => {
     const decrement = () => {
         setNumber(number - 1);
     }
+
+    const [food, setFood] = useState([]);
+
+    const getFood = (foodUrl) => {
+        const url = LOCALHOST_URL + foodUrl
+
+        fetch(url)
+            .then((response) => response.json())
+            .then ((data) => setFood(data))
+    }
+
+    useEffect(() => {
+        getFood('newpizza');
+    }, [])
+
     return (
         <>
 
             <div className={style.noveltiesMenu}>
-                <div className={style.cheeseburger}>
-                    <img className={style.burger} src={props.imageUrl} alt="" />
-                    <div>
-                        <h3 className={style.burgerH3}>{props.title}</h3>
-                        <p className={style.noveltiesMenuP}>{props.titleDesc}</p>
-                        <p><b>{props.desc}</b></p>
-                    </div>
-                    <div className={style.btn}>
-                        <button className={style.btn1} onClick={decrement}>-</button>
-                        <p>{number}</p>
-                        <button className={style.btn2} onClick={increment}>+</button>
-                    </div>
-                    <button className={style.btn3}>В КОРЗИНУ</button>
-                </div>
+
+                {
+                    food.map((item) => {
+                        return (
+                            <div className={style.cheeseburger}>
+                                key={item.id}
+                                <img className={style.burger} src={item.img} />
+                                <div>
+                                    <h3 className={style.burgerH3}>{item.name}</h3>
+                                    <p className={style.noveltiesMenuP}>{item.desc}</p>
+                                    <p><b>{item.price}</b></p>
+                                </div>
+                                <div className={style.btn}>
+                                    <button className={style.btn1} onClick={decrement}>-</button>
+                                    <p>{number}</p>
+                                    <button className={style.btn2} onClick={increment}>+</button>
+                                </div>
+                                <button className={style.btn3}>В КОРЗИНУ</button>
+                            </div>
+                        )
+                    })
+                }
             </div>
 
         </>
