da16fdfd31f75f8557ed4c3c55e9c98f4114d3d5
Aizhan <aizh.aidarova@gmail.com>
2022-04-12 16:26:48 +0600

    feat:fix

--- src/Pages/Menu/PizzaProps.jsx
+++ src/Pages/Menu/PizzaProps.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import style from "./PagesMenu.module.css";
+import {LOCALHOST_URL} from "../../AdminPage/Constant/index";
 
 
-const PizzaProps = (props) => {
+const PizzaProps = () => {
     const [count, setCount] = useState(0);
     const increment = () => {
         setCount(count + 1)
@@ -10,23 +11,42 @@ const PizzaProps = (props) => {
     const decrement = () => {
         setCount(count - 1)
     }
+    const [food, setFood] = useState([]);
+
+    const getFood = (foodUrl) => {
+        const url = LOCALHOST_URL + foodUrl
+
+        fetch(url)
+            .then((response) => response.json())
+            .then ((data) => setFood(data))
+    }
+
+    useEffect(() => {
+        getFood('cheeseburger');
+    }, [])
     return (
         <>
             <div>
-                <div className={style.margarita}>
-                    <img className={style.pizzaPic} src={props.imgUrl} alt="Pizza"/>
-                    <div>
-                        <h3 className={style.price}>{props.title}</h3>
-                        <p className={style.recipe}>{props.titleDesc}</p>
-                        <h3 className={style.price1}>{props.desc}</h3>
-                    <div className={style.countBtn}>
-                        <button className={style.countBtn1} onClick={decrement}> - </button>
-                        <p className={style.countP}>{count}</p>
-                        <button className={style.countBtn2} onClick={increment}> + </button>
-                    </div>
-                    <button className={style.basketBtn}>В КОРЗИНУ</button>
-                    </div>
-                </div>
+                {
+                    food.map((item) => {
+                        return(
+                            <div className={style.margarita}>
+                                <img className={style.pizzaPic} src={item.imgUrl} alt="Pizza"/>
+                                <div>
+                                    <h3 className={style.price}>{item.title}</h3>
+                                    <p className={style.recipe}>{item.titleDesc}</p>
+                                    <h3 className={style.price1}>{item.desc}</h3>
+                                    <div className={style.countBtn}>
+                                        <button className={style.countBtn1} onClick={decrement}> - </button>
+                                        <p className={style.countP}>{count}</p>
+                                        <button className={style.countBtn2} onClick={increment}> + </button>
+                                    </div>
+                                    <button className={style.basketBtn}>В КОРЗИНУ</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </>
     );
