db051f69696e6f8646e012496c91ac3d00ce93d2
hellomeerim <eg.meerim@gmail.com>
2022-04-15 14:36:00 +0600

    fix

--- src/Pages/AboutUs/PagesAboutUs.jsx
+++ src/Pages/AboutUs/PagesAboutUs.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./PagesAboutUs.module.css";
-import { about } from "../../Constant/Constant";
+import { LOCALHOST_URL } from "../../AdminPage/Constant";
 
 const PagesAboutUs = () => {
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    const url = LOCALHOST_URL + "/aboutUs";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+  
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
     <>
         <div className={styles.aboutUsWidth} style={{ maxWidth: "1200px" }}>
           <h5>Почему выбирают нас:</h5>
           <div className={styles.fourAboutUs}>
-            {about.map((item) => {
+            {data.map((item) => {
               return (
                   <div className={styles.aboutUs}>
                     <img src={item.img} alt="" />
