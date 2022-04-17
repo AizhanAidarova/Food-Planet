5ba084dc645dcb0656b5eb23bbe88480c123035a
aigerim <aigerimbekbolotova7@gmail.com>
2022-04-17 12:30:54 +0600

    feat:fixed

--- src/Pages/NoveltiesMenu/NoveltiesMenu.jsx
+++ src/Pages/NoveltiesMenu/NoveltiesMenu.jsx
@@ -4,23 +4,47 @@ import style from '../NoveltiesMenu/Novelties.module.css';
 import { LOCALHOST_URL } from '../../AdminPage/Constant';
 
 
-
-const NoveltiesMenu = () => {
-
+const GoodsCard = (item) => {
     const [number, setNumber] = useState(0);
 
     const increment = () => {
-        setNumber(number + 1);
+        setNumber(number + 1)
     }
     const decrement = () => {
-        setNumber(number - 1);
+        if (number >0){
+            setNumber(number - 1)
+        }
     }
+
+
+    return (
+                        
+        <div key={item.id} className={style.cheeseburger}>
+            <img className={style.burger} src={item.img} />
+            <div className={style.divchese}>
+                <h3 className={style.burgerH3}>{item.name}</h3>
+                <p className={style.noveltiesMenuP}>{item.desc}</p>
+                <p><b>{item.price}</b></p>
+            </div>
+            <div className={style.btn}>
+                <button className={style.btn1} onClick={decrement}>-</button>
+                <p>{number}</p>
+                <button className={style.btn2} onClick={increment}>+</button>
+            </div>
+            <button className={style.btn3}>В КОРЗИНУ</button>
+        </div>
+    )
+}
+
+
+
+const NoveltiesMenu = () => {
 
     const [food, setFood] = useState([]);
 
     const getFood = (foodUrl) => {
         const url = LOCALHOST_URL + foodUrl
-
+       
         fetch(url)
             .then((response) => response.json())
             .then((data) => setFood(data))
@@ -29,6 +53,7 @@ const NoveltiesMenu = () => {
     useEffect(() => {
         getFood('/newburger');
     }, [])
+    
 
     return (
         <>
@@ -49,24 +74,7 @@ const NoveltiesMenu = () => {
             </div>
             {
                 food.map((item) => {
-                    return (
-                        
-                            <div className={style.cheeseburger}>
-                                {/* key={item.id} */}
-                                <img className={style.burger} src={item.img} />
-                                <div className={style.divchese}>
-                                    <h3 className={style.burgerH3}>{item.name}</h3>
-                                    <p className={style.noveltiesMenuP}>{item.desc}</p>
-                                    <p><b>{item.price}</b></p>
-                                </div>
-                                <div className={style.btn}>
-                                    <button className={style.btn1} onClick={decrement}>-</button>
-                                    <p>{number}</p>
-                                    <button className={style.btn2} onClick={increment}>+</button>
-                                </div>
-                                <button className={style.btn3}>В КОРЗИНУ</button>
-                            </div>
-                    )
+                   return <GoodsCard item={item}/> 
                 })
             }
         </>
